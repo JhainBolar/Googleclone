81a54e6f2d9e33bcb015632f4b0ca4d55ee823ea
JhainBolar <agbajeakin84@gmail.com>
2021-08-24 00:50:49 +0800

    my ttt commit

--- components/Search.js
+++ components/Search.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../components/styles/Search.module.css';
  import { IoIosSearch } from 'react-icons/io';
  import { BsMicFill } from 'react-icons/bs';
 
 function Search() {
+     const [input, setInput] = useState(" ")
     const search = e => {
         e.preventDefault();
     }
-}
+
 
     return (
         <div>
             <div className={styles.search}>
             <div className={styles.search__input}>
               <IoIosSearch size={28} className={styles.searchicon} />
-            <input />
+            <input value={input} onChange={e => setInput(e.target.value)} />
             <BsMicFill size={25} />
             </div>
             <div className={styles.search__buttons}>
@@ -27,6 +28,6 @@ function Search() {
         </div>
     )
 
-
+    }
 export default Search;
 
